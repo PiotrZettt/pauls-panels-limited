a012f50bbfbb14457b7ea4b74e40dbed6c9b0287
PiotrZettt <piotrzielinski@o2.pl>
2023-03-11 16:56:11 +0000

    First Deploy Commit

--- PaulsPanelsLogistics/settings.py
+++ PaulsPanelsLogistics/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pauls-panels-limited.herokuapp.com']
 
 
 # Application definition
